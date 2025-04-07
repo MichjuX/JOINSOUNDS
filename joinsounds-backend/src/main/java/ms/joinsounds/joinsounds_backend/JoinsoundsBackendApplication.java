aac904b292bc91ec3c27e6a0facba904affde371
MichjuX <70774571+MichjuX@users.noreply.github.com>
2025-04-07 16:43:03 +0200

    Initial commit

---
+++ joinsounds-backend/src/main/java/ms/joinsounds/joinsounds_backend/JoinsoundsBackendApplication.java
@@ -0,0 +1,13 @@
+package ms.joinsounds.joinsounds_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoinsoundsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoinsoundsBackendApplication.class, args);
+	}
+
+}
---
+++ joinsounds-backend/src/test/java/ms/joinsounds/joinsounds_backend/JoinsoundsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ms.joinsounds.joinsounds_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoinsoundsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
