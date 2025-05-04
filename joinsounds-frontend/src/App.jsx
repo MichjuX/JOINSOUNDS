2d438f4acb1f5f9c6dedda45bde1d4b64f9dcdac
MichjuX <70774571+MichjuX@users.noreply.github.com>
2025-05-05 01:04:49 +0200

    froncik działa +-
    TODO: Zmienić backend rejestracji aby rozróżniał rejestracje admina i ziutka. Oraz na froncie dodać formularze odpowiednie.

--- joinsounds-frontend/src/App.jsx
+++ joinsounds-frontend/src/App.jsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Navbar from './components/common/navbar';
+import LoginPage from './components/auth/LoginPage';
+import RegistrationPage from './components/auth/RegistrationPage';
+import FooterComponent from './components/common/footer';
+import UserService from './components/service/UserService';
+import UpdateUser from './components/userspage/UpdateUser';
+import UserManagementPage from './components/userspage/UserManagementPage';
+import ProfilePage from './components/userspage/ProfilePage';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <div className="content" style={{ minHeight: 'calc(100vh - 120px)' }}>
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/profile" element={<ProfilePage />} />
+            <Route path="/register" element={<RegistrationPage />} />
+
+            {/* Check if user is authenticated and admin before rendering admin-only routes */}
+            {UserService.adminOnly() && (
+              <>
+                
+                <Route path="/admin/user-management" element={<UserManagementPage />} />
+                <Route path="/update-user/:userId" element={<UpdateUser />} />
+              </>
+            )}
+            <Route path="*" element={<Navigate to="/login" />} />
+          </Routes>
+        </div>
+        <FooterComponent />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ joinsounds-frontend/src/components/service/UserService.js
@@ -0,0 +1,125 @@
+import axios from 'axios';
+
+class UserService {
+    static BASE_URL = "http://localhost:8080"
+
+    static async login(email, password) {
+        try {
+            const response = await axios.post(`${this.BASE_URL}/auth/login`, { email, password });
+            return response.data;
+        } catch (error) {
+            console.error("Error logging in:", error);
+            throw error;
+        }
+    }
+
+    static async register(userData, token = null) {
+        try {
+            const config = token ? {
+                headers: {
+                    'Authorization': `Bearer ${token}`,
+                    'Content-Type': 'application/json'
+                }
+            } : {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            };
+    
+            const response = await axios.post(`${this.BASE_URL}/auth/register`, userData, config);
+            return response.data;
+        } catch (error) {
+            console.error("Error registering user:", error);
+            throw error;
+        }
+    }
+    static async getAllUsers(token) {
+        try {
+            const response = await axios.get(`${this.BASE_URL}/admin/get-all-users`, {
+                headers: {Authorization: `Bearer ${token}`}
+            });
+            return response.data;
+        } catch (error) {
+            console.error("Error registering user:", error);
+            throw error;
+        }
+    }
+
+    static async getYourProfile(token) {
+        try {
+            const response = await axios.get(`${this.BASE_URL}/adminuser/get-profile`, {
+                headers: {Authorization: `Bearer ${token}`}
+            });
+            return response.data;
+        } catch (error) {
+            console.error("Error registering user:", error);
+            throw error;
+        }
+    }
+
+    static async getUserById(userId, token){
+        try{
+            const response = await axios.get(`${UserService.BASE_URL}/admin/get-users/${userId}`, 
+            {
+                headers: {Authorization: `Bearer ${token}`}
+            })
+            return response.data;
+        }catch(err){
+            throw err;
+        }
+    }
+
+    static async deleteUser(userId, token) {
+        try {
+            const response = await axios.delete(`${this.BASE_URL}/admin/delete/${userId}`, {
+                headers: {Authorization: `Bearer ${token}`}
+            });
+            return response.data;
+        } catch (error) {
+            console.error("Error registering user:", error);
+            throw error;
+        }
+    }
+
+    static async updateUser(userId, userData, token){
+        try{
+            const response = await axios.put(`${UserService.BASE_URL}/admin/update/${userId}`, userData,
+            {
+                headers: {Authorization: `Bearer ${token}`}
+            })
+            return response.data;
+        }catch(err){
+            throw err;
+        }
+    }
+
+    // AUTH CHECKER
+
+    static logout() {
+        localStorage.removeItem("token");
+        localStorage.removeItem("role");
+    }
+   
+    static isAuthenticated() {
+        const token = localStorage.getItem("token");
+        return !!token;
+    }
+
+    static isAdmin() {
+        const role = localStorage.getItem("role");
+        return role === "ADMIN";
+    }
+
+    static isUser() {
+        const role = localStorage.getItem("role");
+        return role === "USER";
+    }
+
+    static adminOnly() {
+        return this.isAuthenticated() && this.isAdmin();
+    }
+
+
+}
+
+export default UserService;
